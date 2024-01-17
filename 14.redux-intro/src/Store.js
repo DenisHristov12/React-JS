dd7df489370180fcdf9ce957ecc8ecc6cf754467
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2024-01-17 22:25:00 +0200

    redux
    learning redux

--- 14.redux-intro/src/Store.js
+++ 14.redux-intro/src/Store.js
@@ -1,32 +1,12 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-    case 'account/requestLoan':
-      if (state.loan > 0) {
-        return state;
-      }
+const store = createStore(rootReducer);
 
-      //   LATER
-      return { ...state, loan: action.payload };
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-    default:
-      return state;
-  }
-}
+export default store;
