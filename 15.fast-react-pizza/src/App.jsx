c6ef16f2431be039c3d9d25196529ec107ebaf5b
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2024-01-24 16:52:39 +0200

    finish the app

--- 15.fast-react-pizza/src/App.jsx
+++ 15.fast-react-pizza/src/App.jsx
@@ -1,15 +1,16 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import Home from './UI/Home';
-import Error from './UI/Error';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
-import Cart from './features/cart/Cart';
-import Order, { loader as orderLoader } from './features/order/Order';
+import Home from "./UI/Home";
+import Error from "./UI/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
-} from './features/order/CreateOrder';
-import AppLayout from './UI/AppLayout';
+} from "./features/order/CreateOrder";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+import AppLayout from "./UI/AppLayout";
 
 const router = createBrowserRouter([
   {
@@ -17,29 +18,30 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/menu',
+        path: "/menu",
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: '/cart',
+        path: "/cart",
         element: <Cart />,
       },
       {
-        path: '/order/new',
+        path: "/order/new",
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: '/order/:orderId',
+        path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
