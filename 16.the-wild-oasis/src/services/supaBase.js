6e9fa1e8e3c176b628599309beffe565bf22181f
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2024-01-27 17:05:55 +0200

    create cabin
    implement create cabin functionality using react hook form library

--- 16.the-wild-oasis/src/services/supaBase.js
+++ 16.the-wild-oasis/src/services/supaBase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kzehhzlyndskbwjexxah.supabase.co';
+export const supabaseUrl = 'https://kzehhzlyndskbwjexxah.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6ZWhoemx5bmRza2J3amV4eGFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYyNzczOTQsImV4cCI6MjAyMTg1MzM5NH0.3oXqzT6IkTyj0B6tO44kk97CFYFMnjM3a4limPUbB_c';
 const supabase = createClient(supabaseUrl, supabaseKey);
