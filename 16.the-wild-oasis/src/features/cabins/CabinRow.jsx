f0574d37b249f442fea87d3da20d534a4a37793c
DenisHristov <48285397+DenisHristov12@users.noreply.github.com>
2024-01-28 18:07:36 +0200

    duplicate cabin

--- 16.the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 16.the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -5,6 +5,8 @@ import { formatCurrency } from '../../utils/helpers';
 import { useState } from 'react';
 import CreateCabinForm from './CreateCabinForm';
 import { useDeleteCabin } from './useDeleteCabin';
+import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
+import { useCreateCabin } from './useCreateCabin';
 
 const TableRow = styled.div`
   display: grid;
@@ -49,6 +51,8 @@ function CabinRow({ cabin }) {
   const [showForm, setShowForm] = useState(false);
   const { isDeleting, deleteCabin } = useDeleteCabin();
 
+  const { isCreating, createCabin } = useCreateCabin();
+
   const {
     id: cabinId,
     name,
@@ -56,7 +60,19 @@ function CabinRow({ cabin }) {
     regularPrice,
     discount,
     image,
+    description,
   } = cabin;
+
+  function handleDuplicate() {
+    createCabin({
+      name: `Copy of ${name}`,
+      maxCapacity,
+      regularPrice,
+      discount,
+      image,
+      description,
+    });
+  }
 
   return (
     <>
@@ -71,13 +87,20 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          <button onClick={() => setShowForm((show) => !show)}>Edit</button>
+          <button
+            disabled={isCreating}
+            onClick={handleDuplicate}>
+            <HiSquare2Stack />
+          </button>
+          <button onClick={() => setShowForm((show) => !show)}>
+            <HiPencil />
+          </button>
+          <button
+            onClick={() => deleteCabin(cabinId)}
+            disabled={isDeleting}>
+            <HiTrash />
+          </button>
         </div>
-        <button
-          onClick={() => deleteCabin(cabinId)}
-          disabled={isDeleting}>
-          Delete
-        </button>
       </TableRow>
       {showForm && <CreateCabinForm cabinToEdit={cabin} />}
     </>
